a16c7473f2be72b65abb264432a5d85607e1a519
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-07-05 09:04:53 -0700

    task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+         return None
+      reverse_list = my_list[::-1]
+      for i in reverse_list:
+          print("{:d}".format(i))
