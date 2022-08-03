5544aeba6b0ea964a5caa8910dc063841449db16
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-08-02 17:19:24 -0700

    task 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,7 +1,38 @@
 #!/usr/bin/python3
-"""Defines a Python class-to-JSON function."""
+"""
+Module: `12-student.py`
+
+"""
 
 
-def class_to_json(obj):
-    """Return the dictionary represntation of a simple data structure."""
-    return obj.__dict__
+class Student:
+    """class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """Constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Prints the dictionary representation of the object"""
+        normal_dict_flag = False
+        if isinstance(attrs, list):
+            for element in attrs:
+                if not isinstance(element, str):
+                    normal_dict_flag = True
+
+        else:
+            normal_dict_flag = True
+
+        if normal_dict_flag:
+            return self.__dict__
+
+        customized_dict = dict()
+
+        for element in attrs:
+            if element in self.__dict__:
+                customized_dict[element] = self.__dict__[element]
+
+        return customized_dict
+
