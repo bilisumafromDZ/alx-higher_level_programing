ad1f2bf352aceaad073a0c9791d8720582387596
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-10-20 10:39:27 -0700

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Algorithm to find peak element in an unsorted array
+"""
+
+def find_peak(list_of_integers):
+    """
+    Algorithm to find peak element in an unsorted array
+    """
+
+    if not list_of_integers:
+        return None
+
+     middle = int((len(list_of_integers) - 1) / 2)
+
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    elif len(list_of_integers) == 2:
+        if list_of_integers[0] > list_of_integers[1]:
+            return list_of_integers[0]
+        else:
+            return list_of_integers[1]
+
+    if list_of_integers[middle] < list_of_integers[middle + 1]:
+        return find_peak(list_of_integers[middle + 1:])
+    else:
+        return find_peak(list_of_integers[:middle + 1])
