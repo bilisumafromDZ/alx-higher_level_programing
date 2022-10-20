74b53c388764dabb52a63aec6666cd8725960451
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-10-20 11:04:33 -0700

    task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Gets the size in bytes from an url
-curl -sI "$1" | grep Allow | cut -d ":" -f 2 | cut -c 2-
-
+# Display all HTTP methods the server of a given URL will accept.
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
