8167dec066d6e9d820ee8e1ece04efb42851c30c
bilisumafromDZ <bilisuma.ambisa@gmail.com>
2022-11-15 16:35:36 -0800

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const fileDest = process.argv[2];
-
-fs.readFile(fileDest, 'utf-8', function (err, data) {
-	if (err) 
-		console.log(err);
-	} else {
-		 process.stdout.write(data);
-	}
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+	console.log(error || content);
 });
